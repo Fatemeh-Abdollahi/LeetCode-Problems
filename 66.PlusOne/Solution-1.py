7223168392442e514dbb597ac1eadde88fca54c0
Fatemeh-Abdollahi <f.abdollahi.yazdi@gmail.com>
2025-04-27 13:36:30 +0330

    Solve problem 66

---
+++ 66.PlusOne/Solution-1.py
@@ -0,0 +1,5 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = int(''.join(map(str, digits))) + 1
+        result = list(map(int, str(number)))
+        return result
